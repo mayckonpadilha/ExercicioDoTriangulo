00fa55801b4aa502552aaec11f768cc9b78a5f17
Mayckon <mayckon_bobz@hotmail.com>
2023-04-06 15:12:52 -0300

    Add project files.

---
+++ ExercicioDoTriangulo.ConsoleApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExercicioDoTriangulo.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo = new Triangulo();
+
+            triangulo.ladoTriangulo01 = 10;
+            triangulo.ladoTriangulo02 = 10;
+            triangulo.ladoTriangulo03 = 10;
+
+            Console.WriteLine("Os valores que voce informou são referentes ao trinagulo" + triangulo.DiferenciandoTipoDeTriangulo());
+        }
+    }
+}
---
+++ ExercicioDoTriangulo.ConsoleApplication/Triangulo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioDoTriangulo.ConsoleApplication
+{
+    class Triangulo
+    {
+        public int ladoTriangulo01, ladoTriangulo02, ladoTriangulo03;
+       
+
+        public string DiferenciandoTipoDeTriangulo()
+        {
+
+            if (ladoTriangulo01 == ladoTriangulo02 && ladoTriangulo02 == ladoTriangulo03)
+            {
+
+                return "Triangulo Equilátero";
+            }
+            else if (ladoTriangulo01 != ladoTriangulo02 && ladoTriangulo02 != ladoTriangulo03 && ladoTriangulo01 != ladoTriangulo03)
+            {
+
+                return "Triangulo Escaleno";
+            }
+            else if (ladoTriangulo01 == ladoTriangulo02 && ladoTriangulo02 != ladoTriangulo03 || 
+                ladoTriangulo01 == ladoTriangulo03 && ladoTriangulo01 != ladoTriangulo02 ||
+                ladoTriangulo02 == ladoTriangulo03 && ladoTriangulo03 != ladoTriangulo01)
+            {
+                return "Triangulo Equilátero";
+            }
+            else
+            {
+                return "Triangulo invalido";
+            }
+            
+        }
+    }
+}
